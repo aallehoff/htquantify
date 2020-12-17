ff662de1d7f2cc9d115e26d8bcedde3df3b4e464
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-12-17 15:12:53 -0500

    Create __fetch_content() method

--- WebContentQuantifier/WebContentQuantifier.py
+++ WebContentQuantifier/WebContentQuantifier.py
@@ -6,4 +6,15 @@ class WebContent():
         self.url = url
     
     # Send HTTP request and store response
+    def __fetch_content(self):
+        with urlopen(self.url) as response:
+            # Check for HTTP errors
+            if response.code not in range(200, 300):
+                raise Exception(f'Encountered HTTP Error: {response.code}, {response.msg}.')
+
+            # Store relevent information
+            self.date = response.getheader('Date')
+            self.type = response.getheader('Content-Type')
+            self.content = response.read()
+
     # Parse content
