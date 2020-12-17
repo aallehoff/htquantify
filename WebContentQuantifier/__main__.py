3fe099c34288ac78afd6a7f723c292bc088b01ec
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-12-17 14:30:24 -0500

    Setup command line argument parsing and validation

---
+++ WebContentQuantifier/__main__.py
@@ -0,0 +1,12 @@
+import argparse
+from urllib.parse import urlparse
+
+# Parse command line arguments
+clarg_parser = argparse.ArgumentParser(description="Quantify the content located at a given URL.")
+clarg_parser.add_argument("url")
+clargs = clarg_parser.parse_args()
+
+# Validate URL
+parsed_url = urlparse(clargs.url)
+if not (parsed_url.scheme and parsed_url.netloc):
+    raise Exception("Invalid URL.")
