15b41e76d5d1f9a94ce8647937b291294f6192a3
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-12-17 16:03:08 -0500

    Tie together CLI and WebContent class

--- WebContentQuantifier/WebContentQuantifier.py
+++ WebContentQuantifier/WebContentQuantifier.py
@@ -1,10 +1,13 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # pylint: disable=import-error
 
 class WebContent():
     # Initialize object
     def __init__(self, url):
         self.url = url
+        self.__fetch_content()
+        self.__parse_content()
+
     
     # Send HTTP request and store response
     def __fetch_content(self):
@@ -12,6 +15,9 @@ class WebContent():
             # Check for HTTP errors
             if response.code not in range(200, 300):
                 raise Exception(f'Encountered HTTP Error: {response.code}, {response.msg}.')
+            else:
+                self.code = response.code
+                self.msg = response.msg
 
             # Store relevent information
             self.date = response.getheader('Date')
@@ -21,3 +27,10 @@ class WebContent():
     # Parse content
     def __parse_content(self):
         self.soup = BeautifulSoup(self.raw, 'html.parser')
+        
+        # Strip out CSS and JavaScript
+        for code in self.soup(["style, script"]):
+            code.decompose()
+
+        # Tokenize textual content
+        self.tokens = self.soup.findAll(text=True)
--- WebContentQuantifier/__main__.py
+++ WebContentQuantifier/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 from urllib.parse import urlparse
 
-import WebContentQuantifier
+from .WebContentQuantifier import WebContent
 
 # Parse command line arguments
 clarg_parser = argparse.ArgumentParser(description="Quantify the content located at a given URL.")
@@ -12,3 +12,6 @@ clargs = clarg_parser.parse_args()
 parsed_url = urlparse(clargs.url)
 if not (parsed_url.scheme and parsed_url.netloc):
     raise Exception("Invalid URL.")
+
+# Instance WebContent class
+wc = WebContent(clargs.url)
