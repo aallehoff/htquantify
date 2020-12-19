51cbe9ffef13874791f899be067a79d29e3b0684
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-12-19 18:52:52 -0500

    Enhance error handling

--- htquantify/__main__.py
+++ htquantify/__main__.py
@@ -1,19 +1,32 @@
 import argparse
+import sys
 from urllib.parse import urlparse
 
 import quantifier
 
 # Parse command line arguments
 clarg_parser = argparse.ArgumentParser(description="Quantify the contents of a hypertext document from a URL.")
-clarg_parser.add_argument("url")
+clarg_parser.add_argument("url", help='Must include protocol, and address')
+clarg_parser.add_argument('--debug', action='store_true')
 clarg_parser.add_argument('-s', '--sort-by', dest='sort_by', choices=['quantity', 'token'], default='quantity')
 clarg_parser.add_argument('-i', '--include', dest='include', choices=['tags', 'text'], default='text')
 clargs = clarg_parser.parse_args()
 
+# Set debug level
+if not clargs.debug:
+    # Suppress tracebacks
+    sys.tracebacklimit = 0
+
 # Validate URL
 parsed_url = urlparse(clargs.url)
+# If either protocol or address is missing
 if not (parsed_url.scheme and parsed_url.netloc):
-    raise Exception("Invalid URL.")
+    # Missing protocol
+    if not parsed_url.scheme:
+        raise ValueError("Invalid URL; missing protocol.")
+    # Missing address
+    elif not parsed_url.netloc:
+        raise ValueError("Invalid URL; missing address.")
 
 # Instance Hypertext class, and quantify
 hyper = quantifier.Hypertext(clargs.url)
