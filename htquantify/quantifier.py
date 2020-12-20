1f3c28552dd6186912cf719449d75513ec709226
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-12-19 21:01:08 -0500

    Improved comments

--- htquantify/quantifier.py
+++ htquantify/quantifier.py
@@ -6,7 +6,8 @@ from rich.console import Console # pylint: disable=import-error
 from rich.table import Table # pylint: disable=import-error
 
 class Hypertext():
-    # Initialize object
+    """Gathers and stores a representation of a Hypertext document."""
+
     def __init__(self, url):
         self.url = url
 
@@ -17,9 +18,9 @@ class Hypertext():
         self.__load_content()
         self.__tokenize_text()
 
-    
-    # Send HTTP request and store response
     def __fetch_content(self):
+        """Opens a URL and saves its content and metadata to the object."""
+
         with urlopen(self.url) as response:
             # Check for HTTP errors
             if response.code not in range(200, 300):
@@ -33,16 +34,20 @@ class Hypertext():
             self.type = response.getheader('Content-Type')
             self.raw = response.read()
 
-    # Parse content
     def __parse_content(self):
+        """Parses HTML"""
+
         self.soup = BeautifulSoup(self.raw, 'html.parser')
         
     def __clean_content(self):
-        # Strip out CSS and JavaScript
+        """Removes CSS and JavaScript from parsed document"""
+
         for code in self.soup(['style', 'script']):
-            code.decompose()
+            code.decompose() # remove without storing
 
     def __load_content(self):
+        """Loads elements and text nodes into object for later use"""
+
         # Load all elements into a list
         self.tags = []
         for element in self.soup.findAll():
@@ -52,6 +57,8 @@ class Hypertext():
         self.text = self.soup.findAll(text=True)
 
     def __tokenize_text(self):
+        """Convert text node data into easily countable format"""
+
         # Naive whitespace and punctuation tokenizer
         tokenizer = re.compile(r'[\s,.!?/;:"()[\]{}<>]+')
 
@@ -70,6 +77,8 @@ class Hypertext():
         self.text = clean
 
     def quantify(self):
+        """Return results of counting tags and tokens"""
+
         quantities = {
             'overall': {
                 'tags': 0,
@@ -85,6 +94,8 @@ class Hypertext():
         return HypertextQuantities(quantities)
 
     def __counter(self, category, attribute, buffer):
+        """Counts the contents of an attribute on the object"""
+
         for item in attribute:
             buffer['overall'][category] += 1
             try:
@@ -94,6 +105,7 @@ class Hypertext():
 
 
 class HypertextQuantities():
+    """Stores and provides methods for manipulating the results of quantification"""
     def __init__(self, q):
         # Seperate overall stats from category stats
         self.__overall = q.pop('overall')
@@ -113,6 +125,8 @@ class HypertextQuantities():
         self.__cats.extend(new_cats)
 
     def __invert_dictionary(self, d):
+        """Non-destructively flips keys and values in a given dictionary"""
+
         inversion = {}
 
         # Loop through the items in the given dictionary
@@ -133,47 +147,68 @@ class HypertextQuantities():
         return inversion
 
     def get_categories(self):
+        """Accessor method for self.__cats"""
         return self.__cats
     
     def get_overall(self):
+        """Accessor method for self.__overall"""
         return self.__overall
 
+
 class Display():
+    """Provides methods for displaying output"""
+
     def __init__(self, site, data, opts):
         self.site = site
         self.data = data
         self.opts = opts
     
     def output(self):
+        """Performs the output of results"""
+
         # Create a table with striped rows
         self.table = Table(title=self.site.url, row_styles=['', 'grey50'])
 
+        # Build and fill table
         self.__build_columns()
         self.__fill_rows()
 
+        # Print to STDOUT
         console = Console()
         console.print(self.table)
 
     def __build_columns(self):
+        """Builds the columns in a rich table"""
+        
+        # QUANTITY
         if self.opts.sort_by == 'quantity':
             self.table.add_column('Quantity', justify='right')
             self.table.add_column('Token', justify='left')
+        # TOKEN
         elif self.opts.sort_by == 'token':
             self.table.add_column('Token', justify='right')
             self.table.add_column('Quantity', justify='left')
 
     def __fill_rows(self):
+        """Fills the rows in a rich table"""
+
+        # QUANTITY
         if self.opts.sort_by == 'quantity':
+            # TEXT
             if self.opts.include == 'text':
                 for k, v in sorted(self.data.text_by_count.items(), reverse=True):
                     self.table.add_row(str(k), ', '.join(v))
+            # TAGS
             elif self.opts.include == 'tags':
                 for k, v in sorted(self.data.tags_by_count.items(), reverse=True):
                     self.table.add_row(str(k), ', '.join(v))
+        # TOKEN
         elif self.opts.sort_by == 'token':
+            # TEXT
             if self.opts.include == 'text':
                 for k, v in sorted(self.data.text.items()):
                     self.table.add_row(k, str(v))
+            # TAGS
             elif self.opts.include == 'tags':
                 for k, v in sorted(self.data.tags.items()):
                     self.table.add_row(k, str(v))
